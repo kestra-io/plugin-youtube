4e8e52dd955c9a3d76a9179d5011ad70e6ea831c
ben8t <pimpaudben@gmail.com>
2025-12-30 15:43:37 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/youtube/package-info.java
+++ src/main/java/io/kestra/plugin/youtube/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     title = "YouTube",
     description = "This sub-group of plugins contains tasks for interacting with YouTube.\n",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = PluginSubGroup.PluginCategory.TOOL,
+    categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.youtube;
 
